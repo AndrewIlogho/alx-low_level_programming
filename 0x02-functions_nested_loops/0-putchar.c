71d02818b974805a5c8d6df5d0e8f6dfbe313a67
AndrewIlogho <iloghoandrew.ai@gmail.com>
2022-12-13 06:53:26 -0800

    programming c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  main - prints _putchar and a new line
+ *  Return: Always 0
+ */
+int main(void)
+
+	_putchar('_');
+	_putchar('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('e');
+	_putcher('r');
+	_putcher('\n');
+
+	return (0);
+
